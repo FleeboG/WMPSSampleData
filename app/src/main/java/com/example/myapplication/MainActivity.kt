6ce382d866dd7344c840eff663667fd93a1179d6
Ernesto Sierra <105014176+FleeboG@users.noreply.github.com>
2024-02-08 11:13:45 -0600

    MainActivity.kt

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -18,11 +18,17 @@ class MainActivity : AppCompatActivity() {
         val textView4 = findViewById<View>(R.id.textView4) as TextView
         val textView5 = findViewById<View>(R.id.textView5) as TextView
         val textView6 = findViewById<View>(R.id.textView6) as TextView
+        
+        //pressure value placeholder/initialization
         val pressureOne = 0
         val pressureTwo = 0
         val pressureThree = 0
         val pressureThreshold = 65
+
+        //calling method to get pressure levels
         val pressureLevels = pressureDetection(pressureOne, pressureTwo, pressureThree)
+
+        //delay method
         val runnable = Runnable {
             textView3.text = "Moisture Detected: " + moistureDetection()
             textView2.text = "Pressure level on sensor one: " + pressureLevels[0]
@@ -35,12 +41,16 @@ class MainActivity : AppCompatActivity() {
             }
             textView.text = "Reading Done."
         }
+        
+        //setting up parameters for the delay method
         val handler = Handler(Looper.getMainLooper())
         handler.postDelayed(runnable, 15000)
         textView.text = "Reading Please Wait..."
     }
 
     companion object {
+
+        //Getting true or false moisture value
         fun moistureDetection(): Boolean {
             // Using the random library class
             val randomNum = Random()
@@ -56,23 +66,28 @@ class MainActivity : AppCompatActivity() {
             return moisture
         }
 
+        //getting random pressure value
         fun pressureDetection(
             pressureNumOne: Int,
             pressureNumTwo: Int,
             pressureNumThree: Int
         ): IntArray {
+
+            // Initializing pressure limit, threshold, and sensors
             // How we get randomized integer
             var pressureNumOne = pressureNumOne
             var pressureNumTwo = pressureNumTwo
             var pressureNumThree = pressureNumThree
             val pressureRand = Random()
 
-            // Initializing pressure limit, threshold, and sensors
+           // How we get randomized integer
             val pressureLimit = 101
             val pressureThreshold = 65
             pressureNumOne = pressureRand.nextInt(pressureLimit)
             pressureNumTwo = pressureRand.nextInt(pressureLimit)
             pressureNumThree = pressureRand.nextInt(pressureLimit)
+
+            //adding in the random values and putting them into an array
             val arr = IntArray(3)
             arr[0] = pressureNumOne
             arr[1] = pressureNumTwo
@@ -82,6 +97,8 @@ class MainActivity : AppCompatActivity() {
             println("Pressure level on sensor one: $pressureNumOne")
             println("Pressure level on sensor two: $pressureNumTwo")
             println("Pressure level on sensor three: $pressureNumThree")
+
+            //return random values
             return arr
         }
     }
